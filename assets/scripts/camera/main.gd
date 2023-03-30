1e14f4a9e29e24e1fe93bd5291b13a0600184abc
devn1x <16466128+devn1x@users.noreply.github.com>
2023-03-30 22:46:06 +0200

    Add zoom ability to camera script

--- assets/scripts/camera/main.gd
+++ assets/scripts/camera/main.gd
@@ -1,11 +1,20 @@
 extends Camera2D
 
+@export var zoom_step_size: float = 0.1
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _unhandled_input(event):
+	var zoom_pos: Vector2 = Vector2(0, 0)
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				zoom_pos = get_global_mouse_position()
+				self.zoom = Vector2(self.zoom.x * (1 + zoom_step_size), self.zoom.y * (1 + zoom_step_size))
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				zoom_pos = get_global_mouse_position()
+				self.zoom = Vector2(self.zoom.x * (1 - zoom_step_size), self.zoom.y * (1 - zoom_step_size))
