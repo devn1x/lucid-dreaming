b9134fc4f825e9d520f65666bfb8e697cf723197
devn1x <16466128+devn1x@users.noreply.github.com>
2023-03-31 00:06:08 +0200

    Add ability to move camera by key presses

--- assets/scripts/camera/main.gd
+++ assets/scripts/camera/main.gd
@@ -1,22 +1,32 @@
 extends Camera2D
 
 @export var zoom_step_size: float = 0.1
+@export var keyboard_move_speed: float = 7
 
-# Variables for calculating mouse dragging
-var is_dragging = false
+# Variables for calculating camera movement via mouse
+var is_dragging: bool = false
 var drag_from_pos: Vector2 = Vector2.ZERO
 
+# Variables for calculating camera movement via keyboard
+var keyboard_move_direction: Vector2 = Vector2.ZERO
+var keyboard_move_state_up: bool = false
+var keyboard_move_state_down: bool = false
+var keyboard_move_state_left: bool = false
+var keyboard_move_state_right: bool = false
+
 func _ready():
-	pass
+	position_smoothing_enabled = false
 
-func _process(delta):
-	pass
+func _process(delta: float):
+	# TODO: Bring delta into this, so movement is independent of framerate
+	position += (keyboard_move_direction * keyboard_move_speed) / zoom.x
 
-func _unhandled_input(event):
+func _unhandled_input(event: InputEvent):
 	handle_zoom(event)
-	handle_dragging(event)
+	handle_movement_mouse(event)
+	handle_movement_keyboard(event)
 
-func handle_zoom(event):
+func handle_zoom(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
@@ -24,7 +34,7 @@ func handle_zoom(event):
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 				zoom_out(get_global_mouse_position())
 
-func handle_dragging(event):
+func handle_movement_mouse(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			is_dragging = true
@@ -34,8 +44,31 @@ func handle_dragging(event):
 	if event is InputEventMouseMotion and is_dragging:
 		position = drag_from_pos - get_local_mouse_position()
 
-func zoom_in(zoom_pos):
+func handle_movement_keyboard(event: InputEvent):
+	if event is InputEventKey:
+		match event.keycode:
+			KEY_W:
+				keyboard_move_state_up = event.is_pressed()
+			KEY_S:
+				keyboard_move_state_down = event.is_pressed()
+			KEY_A:
+				keyboard_move_state_left = event.is_pressed()
+			KEY_D:
+				keyboard_move_state_right = event.is_pressed()
+	
+	keyboard_move_direction = Vector2.ZERO
+	if keyboard_move_state_up and not keyboard_move_state_down:
+		keyboard_move_direction.y = -1
+	if keyboard_move_state_down and not keyboard_move_state_up:
+		keyboard_move_direction.y = 1
+	if keyboard_move_state_left and not keyboard_move_state_right:
+		keyboard_move_direction.x = -1
+	if keyboard_move_state_right and not keyboard_move_state_left:
+		keyboard_move_direction.x = 1
+	keyboard_move_direction = keyboard_move_direction.normalized()
+
+func zoom_in(zoom_pos: Vector2):
 	self.zoom = Vector2(self.zoom.x * (1 + zoom_step_size), self.zoom.y * (1 + zoom_step_size))
 
-func zoom_out(zoom_pos):
+func zoom_out(zoom_pos: Vector2):
 	self.zoom = Vector2(self.zoom.x * (1 - zoom_step_size), self.zoom.y * (1 - zoom_step_size))
